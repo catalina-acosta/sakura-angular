c2f2822e5e16245bfd7aff7e21fc693b63fc0ffd
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-02-12 09:58:26 +0100

    upload to github

---
+++ src/app/imprint/imprint.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-imprint',
+  imports: [CommonModule],
+  templateUrl: './imprint.component.html',
+  styleUrl: './imprint.component.scss'
+})
+export class ImprintComponent {
+
+}
---
+++ src/app/main-content/landing-page/landing-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+@Component({
+  selector: 'app-landing-page',
+  imports: [NavbarComponent],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.scss'
+})
+export class LandingPageComponent {
+
+}
